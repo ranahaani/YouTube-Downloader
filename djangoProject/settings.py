403e5eb614f6cb51101f30cdb1d35e5c273ba600
Your Name <ranahaani@gmail.com>
2020-10-27 11:45:41 +0500

    UI changed

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -128,3 +128,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static_serve', 'media_root')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_files")]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
